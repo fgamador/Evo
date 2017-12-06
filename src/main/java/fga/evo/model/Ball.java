3f1a7c3f5e2fc9b8654ff1ee733362936a61dab0
Franz Amador <franzamador@gmail.com>
2017-12-05 21:29:53 -0800

    REFACTOR:Intellij - pull methods up

--- src/main/java/fga/evo/model/Ball.java
+++ src/main/java/fga/evo/model/Ball.java
@@ -21,34 +21,6 @@ public class Ball extends NewtonianBody {
     private DecayingAccumulator overlapAccumulator = new DecayingAccumulator(overlapAccumulatorRetentionRate);
 
     /**
-     * Sets the ball's initial position. All subsequent updates to position should be done by {@link #subtickPhysics(int)}.
-     */
-    public void setCenterPosition(double centerX, double centerY) {
-        this.centerX = centerX;
-        this.centerY = centerY;
-    }
-
-    /**
-     * Sets the ball's initial velocity. All subsequent updates to velocity should be done by {@link #subtickPhysics(int)}.
-     */
-    void setVelocity(double velocityX, double velocityY) {
-        this.velocityX = velocityX;
-        this.velocityY = velocityY;
-    }
-
-    /**
-     * Adds a force on the ball that will be used by the next call to {@link #subtickPhysics(int)}. This is the only way to
-     * influence the ball's motion (after setting its initial position and possibly velocity).
-     *
-     * @param forceX X-component of the force
-     * @param forceY Y-component of the force
-     */
-    void addForce(double forceX, double forceY) {
-        netForceX += forceX;
-        netForceY += forceY;
-    }
-
-    /**
      * Updates the ball's velocity and position per the forces currently on it, then clears the forces.
      *
      * @param subticksPerTick time resolution
@@ -59,38 +31,6 @@ public class Ball extends NewtonianBody {
         updatePosition(subticksPerTick);
         clearForces();
         overlapAccumulator.decay();
-    }
-
-    private void updateVelocity(int subticksPerTick) {
-        assert getMass() > 0;
-
-        // the acceleration to apply instantaneously at the beginning of this subtick
-        double accelerationX = netForceX / getMass();
-        double accelerationY = netForceY / getMass();
-
-        // the velocity during this subtick
-        velocityX += accelerationX / subticksPerTick;
-        velocityY += accelerationY / subticksPerTick;
-    }
-
-    // numerical/discretization problems can cause extreme velocities; cap them
-    private void limitSpeed() {
-        double speedSquared = sqr(velocityX) + sqr(velocityY);
-        double maxSpeed = speedLimit.getValue();
-        if (speedSquared > sqr(maxSpeed)) {
-            double throttling = maxSpeed / Math.sqrt(speedSquared);
-            velocityX *= throttling;
-            velocityY *= throttling;
-        }
-    }
-
-    private void updatePosition(int subticksPerTick) {
-        // the position at the end of this subtick
-        setCenterPosition(centerX + (velocityX / subticksPerTick), centerY + (velocityY / subticksPerTick));
-    }
-
-    private void clearForces() {
-        netForceX = netForceY = 0;
     }
 
     /**
@@ -179,38 +119,6 @@ public class Ball extends NewtonianBody {
 
     public double getArea() {
         return area;
-    }
-
-    public void setMass(double val) {
-        mass = val;
-    }
-
-    public double getMass() {
-        return mass;
-    }
-
-    public double getCenterX() {
-        return centerX;
-    }
-
-    public double getCenterY() {
-        return centerY;
-    }
-
-    public double getVelocityX() {
-        return velocityX;
-    }
-
-    public double getVelocityY() {
-        return velocityY;
-    }
-
-    public double getNetForceX() {
-        return netForceX;
-    }
-
-    public double getNetForceY() {
-        return netForceY;
     }
 
     public void onOverlap(double overlap) {
--- src/main/java/fga/evo/model/physics/NewtonianBody.java
+++ src/main/java/fga/evo/model/physics/NewtonianBody.java
@@ -2,6 +2,8 @@ package fga.evo.model.physics;
 
 import fga.evo.model.DoubleParameter;
 
+import static fga.evo.model.Util.sqr;
+
 public class NewtonianBody {
     public static DoubleParameter speedLimit = new DoubleParameter(4);
 
@@ -12,4 +14,96 @@ public class NewtonianBody {
     protected double velocityY;
     protected double netForceX;
     protected double netForceY;
+
+    /**
+     * Sets the ball's initial position. All subsequent updates to position should be done by {@link #subtickPhysics(int)}.
+     */
+    public void setCenterPosition(double centerX, double centerY) {
+        this.centerX = centerX;
+        this.centerY = centerY;
+    }
+
+    /**
+     * Sets the ball's initial velocity. All subsequent updates to velocity should be done by {@link #subtickPhysics(int)}.
+     */
+    public void setVelocity(double velocityX, double velocityY) {
+        this.velocityX = velocityX;
+        this.velocityY = velocityY;
+    }
+
+    /**
+     * Adds a force on the ball that will be used by the next call to {@link #subtickPhysics(int)}. This is the only way to
+     * influence the ball's motion (after setting its initial position and possibly velocity).
+     *
+     * @param forceX X-component of the force
+     * @param forceY Y-component of the force
+     */
+    public void addForce(double forceX, double forceY) {
+        netForceX += forceX;
+        netForceY += forceY;
+    }
+
+    protected void updateVelocity(int subticksPerTick) {
+        assert getMass() > 0;
+
+        // the acceleration to apply instantaneously at the beginning of this subtick
+        double accelerationX = netForceX / getMass();
+        double accelerationY = netForceY / getMass();
+
+        // the velocity during this subtick
+        velocityX += accelerationX / subticksPerTick;
+        velocityY += accelerationY / subticksPerTick;
+    }
+
+    // numerical/discretization problems can cause extreme velocities; cap them
+    protected void limitSpeed() {
+        double speedSquared = sqr(velocityX) + sqr(velocityY);
+        double maxSpeed = speedLimit.getValue();
+        if (speedSquared > sqr(maxSpeed)) {
+            double throttling = maxSpeed / Math.sqrt(speedSquared);
+            velocityX *= throttling;
+            velocityY *= throttling;
+        }
+    }
+
+    protected void updatePosition(int subticksPerTick) {
+        // the position at the end of this subtick
+        setCenterPosition(centerX + (velocityX / subticksPerTick), centerY + (velocityY / subticksPerTick));
+    }
+
+    protected void clearForces() {
+        netForceX = netForceY = 0;
+    }
+
+    public void setMass(double val) {
+        mass = val;
+    }
+
+    public double getMass() {
+        return mass;
+    }
+
+    public double getCenterX() {
+        return centerX;
+    }
+
+    public double getCenterY() {
+        return centerY;
+    }
+
+    public double getVelocityX() {
+        return velocityX;
+    }
+
+    public double getVelocityY() {
+        return velocityY;
+    }
+
+    public double getNetForceX() {
+        return netForceX;
+    }
+
+    public double getNetForceY() {
+        return netForceY;
+    }
 }
