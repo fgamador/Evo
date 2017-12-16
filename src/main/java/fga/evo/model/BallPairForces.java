790f1debf1bfbb596a68c3b1019895bb0dc46213
Franz Amador <franzamador@gmail.com>
2017-12-16 11:25:15 -0800

    REFACTOR:IntelliJ - inline var use

--- src/main/java/fga/evo/model/BallPairForces.java
+++ src/main/java/fga/evo/model/BallPairForces.java
@@ -54,7 +54,7 @@ public class BallPairForces {
     private static void addCollisionForces(Ball ball1, Ball ball2) {
         double centerSeparationSquared = calcCenterSeparationSquared(ball1, ball2);
         double centerSeparation = Math.sqrt(centerSeparationSquared);
-        if (ball1.getRadius() + ball2.getRadius() - centerSeparation > 0) {
+        if (ball1.getRadius() + ball2.getRadius() - Math.sqrt(centerSeparationSquared) > 0) {
             double overlap = ball1.getRadius() + ball2.getRadius() - centerSeparation;
             ball1.onOverlap(overlap);
             ball2.onOverlap(overlap);
