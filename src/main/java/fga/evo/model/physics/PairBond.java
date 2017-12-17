e8932373af1a014130b070f70b43e8ea81ce31ee
Franz Amador <franzamador@gmail.com>
2017-12-16 17:58:58 -0800

    Stop IntelliJ complaining

--- src/main/java/fga/evo/model/physics/PairBond.java
+++ src/main/java/fga/evo/model/physics/PairBond.java
@@ -3,14 +3,22 @@ package fga.evo.model.physics;
 public class PairBond {
     private final NewtonianBody body1;
     private final NewtonianBody body2;
-    private double restLength = 0;
+//    private double restLength = 0;
 
     public PairBond(NewtonianBody body1, NewtonianBody body2) {
         this.body1 = body1;
         this.body2 = body2;
     }
 
-    public void setRestLength(double val) {
-        restLength = val;
+//    public void setRestLength(double val) {
+//        restLength = val;
+//    }
+
+    public NewtonianBody getBody1() {
+        return body1;
+    }
+
+    public NewtonianBody getBody2() {
+        return body2;
     }
 }
