885e7dc73a2b937f396f468e61237ab6001e8c29
Franz Amador <franzamador@gmail.com>
2017-12-23 13:12:59 -0800

    REFACTOR:IntelliJ - inline method

--- src/main/java/fga/evo/model/OverlapDetection.java
+++ src/main/java/fga/evo/model/OverlapDetection.java
@@ -22,22 +22,19 @@ class OverlapDetection {
     }
 
     void addCollisionForces() {
-        addCollisionForces(balls);
-    }
-
-    private void addCollisionForces(List<? extends Ball> balls) {
         for (int i = 0; i < balls.size(); i++) {
-            Ball ball = balls.get(i);
+            Ball ball = ((List<? extends Ball>) balls).get(i);
 
             // TODO Idea: keep balls sorted by centerX. Need check a ball against
             // only those others with greater indexes until we find another ball
             // whose centerX is beyond the max radius plus the first ball's
             // radius. Works for finding shadowing, too.
             for (int j = i + 1; j < balls.size(); j++) {
-                Ball ball2 = balls.get(j);
+                Ball ball2 = ((List<? extends Ball>) balls).get(j);
                 if (!ball.isBondedTo(ball2))
                     PairCollision.addForces(ball, ball2);
             }
         }
     }
+
 }
