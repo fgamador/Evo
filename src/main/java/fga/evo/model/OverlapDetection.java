56ec002dd52d16a8218e8bcd5bb6522dcda38f60
Franz Amador <franzamador@gmail.com>
2017-12-23 13:14:14 -0800

    REFACTOR:minor - removed unnecessary casts

--- src/main/java/fga/evo/model/OverlapDetection.java
+++ src/main/java/fga/evo/model/OverlapDetection.java
@@ -23,14 +23,14 @@ class OverlapDetection {
 
     void addCollisionForces() {
         for (int i = 0; i < balls.size(); i++) {
-            Ball ball = ((List<? extends Ball>) balls).get(i);
+            Ball ball = balls.get(i);
 
             // TODO Idea: keep balls sorted by centerX. Need check a ball against
             // only those others with greater indexes until we find another ball
             // whose centerX is beyond the max radius plus the first ball's
             // radius. Works for finding shadowing, too.
             for (int j = i + 1; j < balls.size(); j++) {
-                Ball ball2 = ((List<? extends Ball>) balls).get(j);
+                Ball ball2 = balls.get(j);
                 if (!ball.isBondedTo(ball2))
                     PairCollision.addForces(ball, ball2);
             }
