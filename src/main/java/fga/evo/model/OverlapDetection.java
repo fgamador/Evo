384260d3ecc3310708eef742911fa52ba43ff5c5
Franz Amador <franzamador@gmail.com>
2017-12-23 11:56:44 -0800

    REFACTOR:minor - change vars to base type

--- src/main/java/fga/evo/model/OverlapDetection.java
+++ src/main/java/fga/evo/model/OverlapDetection.java
@@ -6,16 +6,16 @@ import fga.evo.model.physics.PairCollision;
 import java.util.List;
 
 class OverlapDetection {
-    public void addCollisionForces(List<Cell> cells) {
-        for (int i = 0; i < cells.size(); i++) {
-            Ball ball = cells.get(i);
+    public void addCollisionForces(List<? extends Ball> balls) {
+        for (int i = 0; i < balls.size(); i++) {
+            Ball ball = balls.get(i);
 
-            // TODO Idea: keep cells sorted by centerX. Need check a ball against
+            // TODO Idea: keep balls sorted by centerX. Need check a ball against
             // only those others with greater indexes until we find another ball
             // whose centerX is beyond the max radius plus the first ball's
             // radius. Works for finding shadowing, too.
-            for (int j = i + 1; j < cells.size(); j++) {
-                Ball ball2 = cells.get(j);
+            for (int j = i + 1; j < balls.size(); j++) {
+                Ball ball2 = balls.get(j);
                 if (!ball.isBondedTo(ball2))
                     PairCollision.addForces(ball, ball2);
             }
