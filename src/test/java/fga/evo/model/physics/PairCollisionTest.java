42fae3a6c1d0e2617afe9f501f80613137c61256
Franz Amador <franzamador@gmail.com>
2017-12-24 12:00:47 -0800

    REFACTOR:risky - make test explicit

--- src/test/java/fga/evo/model/physics/PairCollisionTest.java
+++ src/test/java/fga/evo/model/physics/PairCollisionTest.java
@@ -7,30 +7,25 @@ import static fga.evo.model.Assert.assertNetForce;
 import static org.junit.Assert.*;
 
 public class PairCollisionTest {
-    private Ball ball, ball2;
-
     @Before
     public void setUp() {
         Ball.overlapForceFactor.setValue(1);
         BallPairForces.dampingForceFactor.setValue(1);
-
-        ball = new Ball();
-        ball.setRadius(1);
-        ball.setMass(1);
-        ball.setCenterPosition(0, 0);
-
-        ball2 = new Ball();
-        ball2.setRadius(1);
-        ball2.setMass(1);
     }
 
     @Test
-    public void testAddBallPairForces_XRestLength() {
+    public void grazingCollisionAddsNoForces() {
+        Ball ball1 = new Ball();
+        ball1.setRadius(1);
+        ball1.setCenterPosition(0, 0);
+
+        Ball ball2 = new Ball();
+        ball2.setRadius(1);
         ball2.setCenterPosition(2, 0);
 
-        PairCollision.addForces(ball, ball2);
+        PairCollision.addForces(ball1, ball2);
 
-        assertNetForce(0, 0, ball);
+        assertNetForce(0, 0, ball1);
         assertNetForce(0, 0, ball2);
     }
 }
