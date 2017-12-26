b666dca1483bbdefc9e5d02a234d1f892aa2813a
Franz Amador <franzamador@gmail.com>
2017-12-25 17:30:20 -0800

    REFACTOR:minor - delete unused method

--- src/main/java/fga/evo/model/geometry/Circles.java
+++ src/main/java/fga/evo/model/geometry/Circles.java
@@ -3,11 +3,6 @@ package fga.evo.model.geometry;
 import static fga.evo.model.Util.sqr;
 
 public class Circles {
-    public static boolean circlesOverlapWithOffset(Circle circle1, Circle circle2) {
-        double centerSeparationSquared = calcCenterSeparationSquared(circle1, circle2);
-        return centerSeparationSquared != 0 && circlesOverlap(circle1, circle2, centerSeparationSquared);
-    }
-
     public static boolean circlesOverlap(Circle circle1, Circle circle2, double centerSeparationSquared) {
         return sqr(circle1.getRadius() + circle2.getRadius()) > centerSeparationSquared;
     }
