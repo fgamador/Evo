4986235097451948b5b35555e8fbc66b18f4bb6e
Franz Amador <franzamador@gmail.com>
2017-12-25 17:13:18 -0800

    Deleted moved tests

--- src/test/java/fga/evo/model/physics/PairCollisionTest.java
+++ src/test/java/fga/evo/model/physics/PairCollisionTest.java
@@ -14,22 +14,6 @@ public class PairCollisionTest {
         BallPairForces.dampingForceFactor.setValue(1);
     }
 
-//    @Test
-//    public void justTouchingAddsNoForces() {
-//        Ball ball1 = new Ball();
-//        ball1.setRadius(1);
-//        ball1.setCenterPosition(0, 0);
-//
-//        Ball ball2 = new Ball();
-//        ball2.setRadius(1);
-//        ball2.setCenterPosition(2, 0);
-//
-//        PairCollision.addForces(ball1, ball2);
-//
-//        assertNetForce(0, 0, ball1);
-//        assertNetForce(0, 0, ball2);
-//    }
-
     @Test
     public void xOverlapAddsXForces() {
         Ball ball1 = new Ball();
@@ -45,22 +29,6 @@ public class PairCollisionTest {
         assertNetForce(-1, 0, ball1);
         assertNetForce(1, 0, ball2);
     }
-
-//    @Test
-//    public void notTouchingAddsNoForces() {
-//        Ball ball1 = new Ball();
-//        ball1.setRadius(1);
-//        ball1.setCenterPosition(0, 0);
-//
-//        Ball ball2 = new Ball();
-//        ball2.setRadius(1);
-//        ball2.setCenterPosition(3, -3);
-//
-//        PairCollision.addForces(ball1, ball2);
-//
-//        assertNetForce(0, 0, ball1);
-//        assertNetForce(0, 0, ball2);
-//    }
 
     @Test
     public void xyOverlapAddsXYForces() {
