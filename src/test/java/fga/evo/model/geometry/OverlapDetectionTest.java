179ffddc3124ab1912ccbb7af2549edd9055a282
Franz Amador <franzamador@gmail.com>
2017-12-31 17:19:48 -0800

    Replace old way of testing overlap amount

--- src/test/java/fga/evo/model/geometry/OverlapDetectionTest.java
+++ src/test/java/fga/evo/model/geometry/OverlapDetectionTest.java
@@ -95,8 +95,7 @@ public class OverlapDetectionTest {
         testSubject.addCircles(Arrays.asList(circle1, circle2));
         testSubject.findAndNotifyOverlaps();
 
-        assertEquals(0.5, circle1.lastOverlap, 0);
-        assertEquals(-1, circle2.lastOverlap, 0);
+        assertEquals(0.5, getOverlap(circle1, circle2), Assert.DEFAULT_DELTA);
     }
 
     @Test
@@ -108,25 +107,28 @@ public class OverlapDetectionTest {
         testSubject.addCircles(Arrays.asList(circle1, circle2));
         testSubject.findAndNotifyOverlaps();
 
-        assertEquals(1, circle1.lastOverlap, Assert.DEFAULT_DELTA);
-        assertEquals(-1, circle2.lastOverlap, 0);
+        assertEquals(1, getOverlap(circle1, circle2), Assert.DEFAULT_DELTA);
     }
 
     private void recordOverlap(Circle circle1, Circle circle2, double overlap) {
         Set<Circle> key = new HashSet<>(Arrays.asList(circle1, circle2));
-        overlaps.put(key, overlap);
+        assertNull(overlaps.put(key, overlap));
     }
 
-    private boolean foundOverlap(SpyCircle circle1, SpyCircle circle2) {
+    private boolean foundOverlap(Circle circle1, Circle circle2) {
         Set<Circle> key = new HashSet<>(Arrays.asList(circle1, circle2));
         return overlaps.containsKey(key);
+    }
+
+    private double getOverlap(Circle circle1, Circle circle2) {
+        Set<Circle> key = new HashSet<>(Arrays.asList(circle1, circle2));
+        return overlaps.get(key);
     }
 
     private class SpyCircle implements OverlapDetection.Circle {
         private double radius;
         private double centerX;
         private double centerY;
-        double lastOverlap = -1;
 
         SpyCircle(double radius, double centerX, double centerY) {
             this.radius = radius;
@@ -152,7 +154,6 @@ public class OverlapDetectionTest {
         @Override
         public void onOverlap(OverlapDetection.Circle circle, double overlap) {
             recordOverlap(this, circle, overlap);
-            lastOverlap = overlap;
         }
     }
 }
