b11c9090036f9dd308768fd38a85d45ab5a708ab
Franz Amador <franzamador@gmail.com>
2018-01-21 13:23:29 -0800

    Made test more explicit

--- src/test/java/fga/evo/model/geometry/RingTest.java
+++ src/test/java/fga/evo/model/geometry/RingTest.java
@@ -17,14 +17,16 @@ public class RingTest extends EvoTest {
     @Test
     public void settingAreaClearsOuterRadius() {
         TestRing testSubject = new TestRing(1);
+        testSubject.setOuterRadius(2);
+
         testSubject.setArea(Math.PI);
+
         assertEquals(0, testSubject.getOuterRadius(), 0);
     }
 
     @Test
     public void testSetOuterRadius() {
         TestRing ring1 = new TestRing(1);
-        ring1.syncFields(null);
 
         ring1.setOuterRadius(2);
 
