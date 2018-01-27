3f40bf33210daa36bebebf6c589417b3a2fd35c1
Franz Amador <franzamador@gmail.com>
2018-01-26 21:22:43 -0800

    REFACTOR:IntelliJ - rename param

--- src/main/java/fga/evo/model/geometry/Ring.java
+++ src/main/java/fga/evo/model/geometry/Ring.java
@@ -24,9 +24,9 @@ public class Ring {
         setArea(getArea() + deltaArea);
     }
 
-    public void updateRadii(double innerRingOuterRadius) {
-        innerRadius = innerRingOuterRadius;
-        outerRadius = Math.sqrt(sqr(innerRadius) + area / Math.PI);
+    public void updateRadii(double innerRadius) {
+        this.innerRadius = innerRadius;
+        outerRadius = Math.sqrt(sqr(this.innerRadius) + area / Math.PI);
     }
 
     public double getOuterRadius() {
