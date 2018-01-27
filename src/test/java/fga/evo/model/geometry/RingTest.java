ab229183a9142c1b7948fbed2a51e968ffa2718f
Franz Amador <franzamador@gmail.com>
2018-01-26 19:21:54 -0800

    REFACTOR:IntelliJ combo - inline param, intro param

--- src/test/java/fga/evo/model/geometry/RingTest.java
+++ src/test/java/fga/evo/model/geometry/RingTest.java
@@ -3,7 +3,6 @@ package fga.evo.model.geometry;
 import fga.evo.model.EvoTest;
 import org.junit.Test;
 
-import static fga.evo.model.util.Util.sqr;
 import static org.junit.Assert.assertEquals;
 
 public class RingTest extends EvoTest {
@@ -23,10 +22,7 @@ public class RingTest extends EvoTest {
 
     @Test
     public void syncFieldsSetsOuterRadiusAndMassFromArea() {
-        Ring testSubject = new TestRing();
-        testSubject.setArea(Math.PI);
-
-        testSubject.syncFields(null);
+        Ring testSubject = createSyncedRing(Math.PI);
 
         assertEquals(Math.PI, testSubject.getArea(), 0.001);
         assertEquals(1, testSubject.getOuterRadius(), 0.001);
@@ -35,7 +31,7 @@ public class RingTest extends EvoTest {
 
     @Test
     public void syncFieldsSetsOuterRadiusInclusiveOfInnerRingOuterRadius() {
-        Ring innerRing = createSyncedRing(1);
+        Ring innerRing = createSyncedRing(Math.PI);
         Ring testSubject = new TestRing();
         testSubject.setArea(3 * Math.PI);
 
@@ -46,7 +42,7 @@ public class RingTest extends EvoTest {
 
     @Test
     public void syncFieldsSetsOuterRadiusOfZeroAreaRingToEqualInnerRingOuterRadius() {
-        Ring innerRing = createSyncedRing(1);
+        Ring innerRing = createSyncedRing(Math.PI);
         Ring testSubject = new TestRing();
 
         testSubject.syncFields(innerRing);
@@ -55,9 +51,9 @@ public class RingTest extends EvoTest {
         assertEquals(0, testSubject.getArea(), 0);
     }
 
-    private Ring createSyncedRing(double outerRadius) {
+    private Ring createSyncedRing(double area) {
         Ring ring = new TestRing();
-        ring.setArea(Math.PI * sqr(outerRadius));
+        ring.setArea(area);
         ring.syncFields(null);
         return ring;
     }
