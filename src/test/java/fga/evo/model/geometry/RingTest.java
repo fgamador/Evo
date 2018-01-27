88d739a2a878c0765297631d4bff19516ee8e191
Franz Amador <franzamador@gmail.com>
2018-01-26 19:18:38 -0800

    REFACTOR:IntelliJ - remove unused import

--- src/test/java/fga/evo/model/geometry/RingTest.java
+++ src/test/java/fga/evo/model/geometry/RingTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 
 import static fga.evo.model.util.Util.sqr;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class RingTest extends EvoTest {
     @Test
