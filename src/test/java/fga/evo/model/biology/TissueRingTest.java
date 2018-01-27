c915c346f7c18b8af7d86af01d9e134da1fabdb1
Franz Amador <franzamador@gmail.com>
2018-01-26 20:41:31 -0800

    REFACTOR:IntelliJ - inline method

--- src/test/java/fga/evo/model/biology/TissueRingTest.java
+++ src/test/java/fga/evo/model/biology/TissueRingTest.java
@@ -24,19 +24,15 @@ public class TissueRingTest extends EvoTest {
 
     @Test
     public void growthUsesAvailableEnergy() {
-        TissueRing testSubject = createRing1();
+        TissueRing testSubject = new TestTissueRing(Math.PI);
         testSubject.requestResize(2);
         assertEquals(2, testSubject.getIntendedEnergyConsumption(), 0);
-    }
-
-    private TestTissueRing createRing1() {
-        return new TestTissueRing(Math.PI);
     }
 
     @Test
     public void growthEnergyConsumptionIsLimitedByMaxGrowthRate() {
         TestTissueRing.parameters.maxGrowthRate.setValue(0.1);
-        TissueRing testSubject = createRing1();
+        TissueRing testSubject = new TestTissueRing(Math.PI);
         double maxDeltaArea = testSubject.getArea() * TestTissueRing.parameters.maxGrowthRate.getValue();
         double maxGrowthEnergy = maxDeltaArea * TestTissueRing.parameters.growthCost.getValue();
         assertTrue(maxGrowthEnergy < 100);
@@ -48,14 +44,14 @@ public class TissueRingTest extends EvoTest {
 
     @Test
     public void shrinkageYieldsRequestedEnergy() {
-        TissueRing testSubject = createRing1();
+        TissueRing testSubject = new TestTissueRing(Math.PI);
         testSubject.requestResize(-0.1);
         assertEquals(-0.1, testSubject.getIntendedEnergyConsumption(), 0);
     }
 
     @Test
     public void shrinkageEnergyYieldIsLimitedByAvailableArea() {
-        TissueRing testSubject = createRing1();
+        TissueRing testSubject = new TestTissueRing(Math.PI);
         double initialArea = testSubject.getArea();
 
         testSubject.requestResize(-5);
@@ -66,7 +62,7 @@ public class TissueRingTest extends EvoTest {
 
     @Test
     public void scalingResizeRequestScalesEnergyConsumption() {
-        TissueRing testSubject = createRing1();
+        TissueRing testSubject = new TestTissueRing(Math.PI);
 
         testSubject.requestResize(10);
         testSubject.scaleResizeRequest(0.1);
@@ -76,7 +72,7 @@ public class TissueRingTest extends EvoTest {
 
     @Test
     public void resizeChangesArea() {
-        TissueRing testSubject = createRing1();
+        TissueRing testSubject = new TestTissueRing(Math.PI);
         double initialArea = testSubject.getArea();
         testSubject.requestResize(2);
 
@@ -88,7 +84,7 @@ public class TissueRingTest extends EvoTest {
 
     @Test
     public void resizeWithoutRequestDoesNothing() {
-        TissueRing testSubject = createRing1();
+        TissueRing testSubject = new TestTissueRing(Math.PI);
         double initialArea = testSubject.getArea();
 
         testSubject.resize();
@@ -98,7 +94,7 @@ public class TissueRingTest extends EvoTest {
 
     @Test
     public void resizeIsIdempotent() {
-        TissueRing testSubject = createRing1();
+        TissueRing testSubject = new TestTissueRing(Math.PI);
         testSubject.requestResize(2);
         testSubject.resize();
         double areaAfterFirstResize = testSubject.getArea();
@@ -110,7 +106,7 @@ public class TissueRingTest extends EvoTest {
 
     @Test
     public void resizeWillNotShrinkBelowZeroArea() {
-        TissueRing testSubject = createRing1();
+        TissueRing testSubject = new TestTissueRing(Math.PI);
         testSubject.requestResize(-5);
 
         testSubject.resize();
@@ -121,7 +117,7 @@ public class TissueRingTest extends EvoTest {
     @Test
     public void shrinkingWithZeroShrinkageYieldParameterShrinksToZeroArea() {
         TestTissueRing.parameters.shrinkageYield.setValue(0);
-        TissueRing testSubject = createRing1();
+        TissueRing testSubject = new TestTissueRing(Math.PI);
         testSubject.requestResize(-0.01);
 
         testSubject.resize();
@@ -131,7 +127,7 @@ public class TissueRingTest extends EvoTest {
 
     @Test
     public void decayUsesExpectedCalculation() {
-        TissueRing testSubject = createRing1();
+        TissueRing testSubject = new TestTissueRing(Math.PI);
         double initialArea = testSubject.getArea();
 
         testSubject.decay();
