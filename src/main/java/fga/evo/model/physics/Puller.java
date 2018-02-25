7d25f082adb590f0d959e2552518a75f0c418b92
Franz Amador <franzamador@gmail.com>
2018-02-25 11:32:54 -0800

    REFACTOR:minor - added unused new ctor

--- src/main/java/fga/evo/model/physics/Puller.java
+++ src/main/java/fga/evo/model/physics/Puller.java
@@ -9,7 +9,13 @@ public class Puller {
     public static DoubleParameter forceFactor = new DoubleParameter(1);
 
     private double x, y;
+    private NewtonianBody pulledBody;
     private NewtonianBodyEnvironment pulledBodyEnvironment;
+
+    public Puller(NewtonianBodyEnvironment pulledBodyEnvironment, NewtonianBody pulledBody) {
+        this.pulledBody = pulledBody;
+        this.pulledBodyEnvironment = pulledBodyEnvironment;
+    }
 
     public Puller(NewtonianBodyEnvironment pulledBodyEnvironment) {
         this.pulledBodyEnvironment = pulledBodyEnvironment;
