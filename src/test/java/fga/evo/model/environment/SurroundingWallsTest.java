8ca99305a756408c5e396f30cf371954d9f38179
Franz Amador <franzamador@gmail.com>
2018-02-25 17:30:35 -0800

    REFACTOR:IntelliJ - extract vars

--- src/test/java/fga/evo/model/environment/SurroundingWallsTest.java
+++ src/test/java/fga/evo/model/environment/SurroundingWallsTest.java
@@ -21,37 +21,41 @@ public class SurroundingWallsTest extends EvoTest {
     @Test
     public void justTouchingAddsNoForce() {
         cell.setCenterPosition(1, -1);
+        CellEnvironment environment = cell.getEnvironment();
 
-        walls.updateEnvironment(cell.getEnvironment(), cell);
+        walls.updateEnvironment(environment, cell);
 
-        assertNetForce(0, 0, cell.getEnvironment());
+        assertNetForce(0, 0, environment);
     }
 
     @Test
     public void lowXYCollisionAddsForce() {
         cell.setCenterPosition(0.5, -0.5);
+        CellEnvironment environment = cell.getEnvironment();
 
-        walls.updateEnvironment(cell.getEnvironment(), cell);
+        walls.updateEnvironment(environment, cell);
 
-        assertNetForce(0.5, -0.5, cell.getEnvironment());
+        assertNetForce(0.5, -0.5, environment);
     }
 
     @Test
     public void highXYCollisionAddsForce() {
         cell.setCenterPosition(9.5, -9.5);
+        CellEnvironment environment = cell.getEnvironment();
 
-        walls.updateEnvironment(cell.getEnvironment(), cell);
+        walls.updateEnvironment(environment, cell);
 
-        assertNetForce(-0.5, 0.5, cell.getEnvironment());
+        assertNetForce(-0.5, 0.5, environment);
     }
 
     @Test
     public void highCeilingAddsNoForce() {
         walls = new SurroundingWalls(-5, 5, -5, 5);
         cell.setCenterPosition(0, 0);
+        CellEnvironment environment = cell.getEnvironment();
 
-        walls.updateEnvironment(cell.getEnvironment(), cell);
+        walls.updateEnvironment(environment, cell);
 
-        assertNetForce(0, 0, cell.getEnvironment());
+        assertNetForce(0, 0, environment);
     }
 }
