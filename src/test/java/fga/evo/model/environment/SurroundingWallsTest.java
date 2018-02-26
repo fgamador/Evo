bfae8953b62922eb0ee453c2743398e6d71aad33
Franz Amador <franzamador@gmail.com>
2018-02-25 17:35:34 -0800

    REFACTOR:risky - move test-case field into tests as vars

--- src/test/java/fga/evo/model/environment/SurroundingWallsTest.java
+++ src/test/java/fga/evo/model/environment/SurroundingWallsTest.java
@@ -9,15 +9,9 @@ import org.junit.Test;
 import static fga.evo.model.Assert.assertNetForce;
 
 public class SurroundingWallsTest extends EvoTest {
-    private SurroundingWalls walls;
-
-    @Before
-    public void setUp() {
-        walls = new SurroundingWalls(0, 10, -10, 0);
-    }
-
     @Test
     public void justTouchingAddsNoForce() {
+        SurroundingWalls walls = new SurroundingWalls(0, 10, -10, 0);
         Cell cell = new Cell(1);
         cell.setCenterPosition(1, -1);
         CellEnvironment environment = new CellEnvironment();
@@ -29,6 +23,7 @@ public class SurroundingWallsTest extends EvoTest {
 
     @Test
     public void lowXYCollisionAddsExpectedForce() {
+        SurroundingWalls walls = new SurroundingWalls(0, 10, -10, 0);
         Cell cell = new Cell(1);
         cell.setCenterPosition(0.5, -0.5);
         CellEnvironment environment = new CellEnvironment();
@@ -40,6 +35,7 @@ public class SurroundingWallsTest extends EvoTest {
 
     @Test
     public void highXYCollisionAddsExpectedForce() {
+        SurroundingWalls walls = new SurroundingWalls(0, 10, -10, 0);
         Cell cell = new Cell(1);
         cell.setCenterPosition(9.5, -9.5);
         CellEnvironment environment = new CellEnvironment();
@@ -51,8 +47,8 @@ public class SurroundingWallsTest extends EvoTest {
 
     @Test
     public void highCeilingAddsNoForce() {
+        SurroundingWalls walls = new SurroundingWalls(-5, 5, -5, 5);
         Cell cell = new Cell(1);
-        walls = new SurroundingWalls(-5, 5, -5, 5);
         cell.setCenterPosition(0, 0);
         CellEnvironment environment = new CellEnvironment();
 
