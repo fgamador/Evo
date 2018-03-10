436ca023612602640e7480c1993c83e00603fc43
Franz Amador <franzamador@gmail.com>
2018-03-09 21:28:19 -0800

    Add default constructor

--- src/test/java/fga/evo/model/physics/NewtonianBodyWithEnvironment.java
+++ src/test/java/fga/evo/model/physics/NewtonianBodyWithEnvironment.java
@@ -3,6 +3,9 @@ package fga.evo.model.physics;
 class NewtonianBodyWithEnvironment extends NewtonianBody {
     NewtonianBodyEnvironment environment = new NewtonianBodyEnvironment();
 
+    public NewtonianBodyWithEnvironment() {
+    }
+
     public NewtonianBodyWithEnvironment(double centerX, double centerY, double velocityX, double velocityY) {
         super(centerX, centerY, velocityX, velocityY);
     }
