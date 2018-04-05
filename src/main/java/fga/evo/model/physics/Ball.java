463ed7df6a6007b63b2b027c1425707bde95fce7
Franz Amador <franzamador@gmail.com>
2018-04-04 20:51:52 -0700

    REFACTOR:format

--- src/main/java/fga/evo/model/physics/Ball.java
+++ src/main/java/fga/evo/model/physics/Ball.java
@@ -107,5 +107,4 @@ public abstract class Ball extends NewtonianBody implements OverlappableCircle {
     public double getRecentTotalOverlap() {
         return overlapAccumulator.getTotal();
     }
-
 }
