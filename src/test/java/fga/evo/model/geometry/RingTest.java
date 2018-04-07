6e6312ae1ac4d86cb3770a93055849d9351685e2
Franz Amador <franzamador@gmail.com>
2018-04-07 16:26:16 -0700

    REFACTOR:risky - minor test polishing

--- src/test/java/fga/evo/model/geometry/RingTest.java
+++ src/test/java/fga/evo/model/geometry/RingTest.java
@@ -3,6 +3,7 @@ package fga.evo.model.geometry;
 import fga.evo.model.EvoTest;
 import org.junit.Test;
 
+import static fga.evo.model.Assert.assertApproxEquals;
 import static org.junit.Assert.assertEquals;
 
 public class RingTest extends EvoTest {
@@ -10,42 +11,42 @@ public class RingTest extends EvoTest {
     public void canSetArea() {
         Ring testSubject = new TestRing(0);
         testSubject.setArea(Math.PI);
-        assertEquals(Math.PI, testSubject.getArea(), 0.001);
+        assertApproxEquals(Math.PI, testSubject.getArea());
     }
 
     @Test
     public void settingAreaAlsoSetsMass() {
         Ring testSubject = new TestRing(0);
         testSubject.setArea(Math.PI);
-        assertEquals(Math.PI * TestRing.parameters.density.getValue(), testSubject.getMass(), 0.001);
+        assertApproxEquals(Math.PI * TestRing.parameters.density.getValue(), testSubject.getMass());
     }
 
     @Test
     public void canAddToArea() {
         Ring testSubject = new TestRing(1);
         testSubject.addToArea(2);
-        assertEquals(3, testSubject.getArea(), 0.001);
+        assertApproxEquals(3, testSubject.getArea());
     }
 
     @Test
     public void canMultiplyAreaByFactor() {
         Ring testSubject = new TestRing(2);
         testSubject.multiplyAreaBy(0.5);
-        assertEquals(1, testSubject.getArea(), 0.001);
+        assertApproxEquals(1, testSubject.getArea());
     }
 
     @Test
     public void canSetOuterRadiusBasedOnArea() {
         Ring testSubject = new TestRing(Math.PI);
         testSubject.setRadiiBasedOnArea(0);
-        assertEquals(1, testSubject.getOuterRadius(), 0.001);
+        assertApproxEquals(1, testSubject.getOuterRadius());
     }
 
     @Test
     public void canSetOuterRadiusBasedOnAreaAndInnerRadius() {
         Ring testSubject = new TestRing(3 * Math.PI);
         testSubject.setRadiiBasedOnArea(1);
-        assertEquals(2, testSubject.getOuterRadius(), 0.001);
+        assertApproxEquals(2, testSubject.getOuterRadius());
     }
 
     @Test
