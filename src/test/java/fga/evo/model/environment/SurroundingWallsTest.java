23e17947bcfb8102ab9845666b403f853b627693
Franz Amador <franzamador@gmail.com>
2018-04-07 17:15:33 -0700

    REFACTOR:risky - minor test polishing

--- src/test/java/fga/evo/model/environment/SurroundingWallsTest.java
+++ src/test/java/fga/evo/model/environment/SurroundingWallsTest.java
@@ -1,56 +1,55 @@
 package fga.evo.model.environment;
 
-import fga.evo.model.biology.Cell;
 import fga.evo.model.EvoTest;
+import fga.evo.model.biology.Cell;
 import fga.evo.model.physics.Ball;
 import fga.evo.model.physics.BallWithEnvironment;
 import org.junit.Test;
 
 import static fga.evo.model.Assert.assertApproxEquals;
 import static fga.evo.model.Assert.assertNetForce;
-import static org.junit.Assert.assertEquals;
 
 public class SurroundingWallsTest extends EvoTest {
     @Test
     public void justTouchingAddsNoForce() {
-        SurroundingWalls walls = new SurroundingWalls(0, 10, -10, 0);
+        SurroundingWalls subject = new SurroundingWalls(0, 10, -10, 0);
         Cell cell = new Cell(1);
         cell.setCenterPosition(1, -1);
 
-        walls.updateEnvironment(cell);
+        subject.updateEnvironment(cell);
 
         assertNetForce(0, 0, cell.getEnvironment());
     }
 
     @Test
     public void lowXYCollisionAddsExpectedForce() {
-        SurroundingWalls walls = new SurroundingWalls(0, 10, -10, 0);
+        SurroundingWalls subject = new SurroundingWalls(0, 10, -10, 0);
         Cell cell = new Cell(1);
         cell.setCenterPosition(0.5, -0.5);
 
-        walls.updateEnvironment(cell);
+        subject.updateEnvironment(cell);
 
         assertNetForce(0.5, -0.5, cell.getEnvironment());
     }
 
     @Test
     public void highXYCollisionAddsExpectedForce() {
-        SurroundingWalls walls = new SurroundingWalls(0, 10, -10, 0);
+        SurroundingWalls subject = new SurroundingWalls(0, 10, -10, 0);
         Cell cell = new Cell(1);
         cell.setCenterPosition(9.5, -9.5);
 
-        walls.updateEnvironment(cell);
+        subject.updateEnvironment(cell);
 
         assertNetForce(-0.5, 0.5, cell.getEnvironment());
     }
 
     @Test
     public void highCeilingAddsNoForce() {
-        SurroundingWalls walls = new SurroundingWalls(-5, 5, -5, 5);
+        SurroundingWalls subject = new SurroundingWalls(-5, 5, -5, 5);
         Cell cell = new Cell(1);
         cell.setCenterPosition(0, 0);
 
-        walls.updateEnvironment(cell);
+        subject.updateEnvironment(cell);
 
         assertNetForce(0, 0, cell.getEnvironment());
     }
