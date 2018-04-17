d90a4f31771d6a8fc09704e74243c4ca78d74df9
Franz Amador <franzamador@gmail.com>
2018-04-16 19:50:58 -0700

    REFACTOR:risky - remove obsolete code

--- src/main/java/fga/evo/model/physics/NewtonianBodyEnvironment.java
+++ src/main/java/fga/evo/model/physics/NewtonianBodyEnvironment.java
@@ -1,30 +1,4 @@
 package fga.evo.model.physics;
 
 public class NewtonianBodyEnvironment {
-    private double netForceX;
-    private double netForceY;
-
-    /**
-     * Adds a force on the body that will be used by the next call to {@link NewtonianBody#subtick}. This is the
-     * only way to influence the body's motion (after setting its initial position and possibly velocity).
-     *
-     * @param forceX X-component of the force
-     * @param forceY Y-component of the force
-     */
-    public void addForce(double forceX, double forceY) {
-        netForceX += forceX;
-        netForceY += forceY;
-    }
-
-    public void clearForces() {
-        netForceX = netForceY = 0;
-    }
-
-    public double getNetForceX() {
-        return netForceX;
-    }
-
-    public double getNetForceY() {
-        return netForceY;
-    }
 }
