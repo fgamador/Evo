5d3c012dce8466313f63f71c2669b2432b631755
Franz Amador <franzamador@gmail.com>
2018-05-08 19:37:29 -0700

    Remove unused import

--- src/test/java/fga/evo/model/biology/CellPairShadowingTest.java
+++ src/test/java/fga/evo/model/biology/CellPairShadowingTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 
 import static fga.evo.model.Assert.assertApproxEquals;
 import static fga.evo.model.Assert.assertExactEquals;
-import static org.junit.Assert.*;
 
 public class CellPairShadowingTest {
     @Test
